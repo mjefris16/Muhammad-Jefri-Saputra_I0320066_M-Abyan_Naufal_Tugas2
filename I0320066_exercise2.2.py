c32b71ac2fbed77cd2c8b249ae954fd4a8752b39
mjefris16 <80145188+mjefris16@users.noreply.github.com>
2021-03-14 23:09:03 +0700

    Add files via upload

---
+++ I0320066_exercise2.2.py
@@ -0,0 +1,31 @@
+#Proses memasukkan data ke dalam variabel
+nama = "Muhammad Jefri Saputra"
+
+#Proses mencetak variabel
+print(nama)
+
+#Nilai dan tipe data dalam variabel dapat di ubah
+umur = 19
+print(umur)
+type(umur)
+umur = "sembilan belas"
+print(umur)
+type(umur)
+
+namaDepan = "Muhammad"
+namaBelakang =" Jefri Saputra"
+nama = namaDepan + namaBelakang
+umur = 19
+hobi = "makan"
+print("Biodata\n", nama, "\n", umur, "\n", hobi)
+
+#Contoh variabel lainnya
+inivariabel = "Hai"
+ini_juga_variabel = "annyeong"
+_inivariabeljuga = "arigatou"
+inivariabel222 = "Bye"
+
+panjang = 10
+lebar = 5
+luas = panjang * lebar
+print(luas)
---
+++ I0320066_exercise2.3.py
@@ -0,0 +1,17 @@
+import math
+
+#Menampilkan informasi program
+print("luas dan keliling lingkaran")
+
+#Input nilai jari-jari
+r = float(input("Masukkan nilai jari-jari: "))
+
+#Menghitung luas lingkaran
+luas_lingkaran = 3.14 * (r ** 2)
+
+#Menghitung keliling lingkaran
+keliling_lingkaran = 2 * 3.14 * r
+
+#Menampilkan hasil perhitungan ke layar
+print("Luas lingkaran: ", luas_lingkaran)
+print("Keliling lingkaran: ", keliling_lingkaran)
